6136de5760ae26d6e92ed22d0382803599b2f8e4
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-04-11 13:58:37 +0300

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
 YEARS_RANGE = (2010..2020).to_a
+LANGUAGES = %w(Arabic Somali Amharic Oromo Igbo Swahili Hausa Manding Fulani Yoruba)
+
+Book.delete_all
 
 100.times do |count|
   Book.create({
-    author_name:      "#{count} Original Name - #{Faker::Book.author}",
-    author_pen_name:  "#{count} Pen Name - #{Faker::Book.author}",
-    book_genre:       "#{count} Genre - #{Faker::Book.genre}",
-    book_original_language: "#{count} Language - #{Faker::Number.between(from: 1, to: 10)}",
-    book_publishing_year: "#{count} Year - #{YEARS_RANGE.sample}",
-    book_rating:      "#{count} Rating - #{Faker::Number.between(from: 1, to: 10)}",
-    book_title:       "#{count} Title - #{Faker::Book.title}",
-    publisher_name:   "#{count} Publisher - #{Faker::Book.publisher}",
-    author_country_of_origin: "#{count} Country - #{Faker::WorldCup.team}",
+    author_name:      "Original Name - #{Faker::Book.author}",
+    author_pen_name:  "Pen Name - #{Faker::Book.author}",
+    book_genre:       "Genre - #{Faker::Book.genre}",
+    book_original_language: "Language - #{LANGUAGES.sample}",
+    book_publishing_year: "Year - #{YEARS_RANGE.sample}",
+    book_rating:      "Rating - #{Faker::Number.between(from: 1, to: 10)}",
+    book_title:       "Title - #{Faker::Book.title}",
+    publisher_name:   "Publisher - #{Faker::Book.publisher}",
+    author_country_of_origin: "Country - #{Faker::WorldCup.team}",
   })
 end
 
