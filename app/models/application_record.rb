823dfed7b3422caca68d973ddab323c2f89b26d4
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-04-17 11:35:09 +0300

    Exclude id, created_at and updated_at when fetching column names

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  COLUMNS_TO_EXCLUDE = %w( id updated_at created_at)
+
+  def self.column_names
+    super - COLUMNS_TO_EXCLUDE
+  end
 end
