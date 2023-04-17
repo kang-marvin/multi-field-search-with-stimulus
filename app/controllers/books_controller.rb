1b53be59f03431b083612f24fb8d5e801b938461
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-04-17 11:36:57 +0300

    Pass the searchable and table fields to UI from controller instead of hardcoding it

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
 
+  SEARCHABLE_FIELDS = [ 'author_name', 'publisher_name', 'book_genre' ]
+
   def index
+    @searchable_fields = SEARCHABLE_FIELDS
+    @table_fields = Book.column_names
     @books = Book.all
   end
 
